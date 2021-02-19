25d32550fefb5bcb091668a21e739110644fa102
nabilgj <nabilgj.bnm@gmail.com>
2021-02-19 09:18:49 -0500

    made changes in Link child component inside brand page

--- pages/brand.js
+++ pages/brand.js
@@ -20,7 +20,7 @@ export default function Shop() {
       <Box sx={{ my: 2 }}>
         <Grid container direction="row" justify="space-evenly">
           <Grid item md>
-            <Link href="/brand/1" passHref={true}>
+            <Link href="/brand/1">
               {/* <Image
                 // src={`${process.env.PUBLIC_URL}1.jpg`}
                 src="https://images.pexels.com/photos/243757/pexels-photo-243757.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
@@ -35,11 +35,12 @@ export default function Shop() {
                 // sizes="100vw"
                 width={400}
                 height={400}
+                passHref={true}
               />
             </Link>
           </Grid>
           <Grid item md>
-            <Link href="/brand/2" passHref={true}>
+            <Link href="/brand/2">
               {/* <Image
                 // src={`${process.env.PUBLIC_URL}two.jpg`}
                 src="https://images.pexels.com/photos/442573/pexels-photo-442573.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
@@ -54,11 +55,12 @@ export default function Shop() {
                 // sizes="100vw"
                 width={400}
                 height={400}
+                passHref={true}
               />
             </Link>
           </Grid>
           <Grid item md>
-            <Link href="/brand/3" passHref={true}>
+            <Link href="/brand/3">
               {/* <Image
                 // src={`${process.env.PUBLIC_URL}three.jpg`}
                 src="https://images.pexels.com/photos/51383/photo-camera-subject-photographer-51383.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
@@ -72,6 +74,7 @@ export default function Shop() {
                 // sizes="100vw"
                 width={400}
                 height={400}
+                passHref={true}
               />
             </Link>
           </Grid>
